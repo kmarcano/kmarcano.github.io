591b8169d5b790135a8a79b98c3e3f72701a7dfa
Keessy <kmarcano@caldwell.edu>
2022-11-01 15:34:21 -0400

    Javascript pt.1

---
+++ helloworld.js
@@ -0,0 +1,6 @@
+onload=function(){this.alert('Hello World')}
+function hello(){alert('Hello dear user...');}
+function curious(){
+    console.log('This is a trial')
+    alert('This is your sign from the Universe <3');
+}
